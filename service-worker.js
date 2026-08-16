1fb1d700d271eef535b8b142daa2ac6fdf3fd0aa
mvagaiemmanuel-gif <mvagaiemmanuel@gmail.com>
2026-08-16 10:33:08 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // Service Worker TJBS Mobile - fonctionnement hors-ligne
-const CACHE_NAME = 'tjbs-cache-v1';
+const CACHE_NAME = 'tjbs-cache-v2';
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
